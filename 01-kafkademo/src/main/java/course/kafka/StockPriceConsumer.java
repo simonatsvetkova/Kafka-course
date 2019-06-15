4bc5a7090f474d34c3c379cababb44841b067b9a
Simona Tsvetkova <simonatsvetkova@gmail.com>
2019-06-15 17:15:04 +0300

    StockPrice Producer and Consumer commit async - v2

--- 01-kafkademo/src/main/java/course/kafka/StockPriceConsumer.java
+++ 01-kafkademo/src/main/java/course/kafka/StockPriceConsumer.java
@@ -46,44 +46,56 @@ public class StockPriceConsumer {
 //                new TopicPartition(PRICES_TOPIC, 1),
 //                new TopicPartition(PRICES_TOPIC, 2)
 //        ));
+        try {
 
-        while(true) {
-            ConsumerRecords<String, StockPrice> records = consumer.poll(Duration.ofMillis(100));
-            if (records.count() > 0) {
-                log.info("Fetched {} records", records.count());
-                for (ConsumerRecord<String, StockPrice> rec : records) {
-                    log.debug("Record - topic {}, partition {}, offset {}, timestamp {}, value: {}",
-                            rec.topic(),
-                            rec.partition(),
-                            rec.offset(),
-                            rec.timestamp(),
-                            rec.value());
-                    log.info("{} -> {}, {}, {}, {}",
-                            rec.key(),
-                            rec.value().getId(),
-                            rec.value().getSymbol(),
-                            rec.value().getName(),
-                            rec.value().getPrice());
-                    int updatedCount = 1;
-                    if (eventMap.containsKey(rec.key())) {
-                        updatedCount = eventMap.get(rec.key()) + 1;
+            while (true) {
+                ConsumerRecords<String, StockPrice> records = consumer.poll(Duration.ofMillis(100));
+                if (records.count() > 0) {
+                    log.info("Fetched {} records", records.count());
+                    for (ConsumerRecord<String, StockPrice> rec : records) {
+                        log.debug("Record - topic {}, partition {}, offset {}, timestamp {}, value: {}",
+                                rec.topic(),
+                                rec.partition(),
+                                rec.offset(),
+                                rec.timestamp(),
+                                rec.value());
+                        log.info("{} -> {}, {}, {}, {}",
+                                rec.key(),
+                                rec.value().getId(),
+                                rec.value().getSymbol(),
+                                rec.value().getName(),
+                                rec.value().getPrice());
+                        int updatedCount = 1;
+                        if (eventMap.containsKey(rec.key())) {
+                            updatedCount = eventMap.get(rec.key()) + 1;
+                        }
+                        eventMap.put(rec.key(), updatedCount);
+
                     }
-                    eventMap.put(rec.key(), updatedCount);
 
+                    consumer.commitAsync((offsets, exception) -> {
+                        if (exception != null) {
+                            log.error("Error committing offsets", exception);
+                            return;
+                        }
+
+                        log.debug("Offsets committed: {}", offsets);
+                    });
+
+                    JSONObject json = new JSONObject(eventMap);
+                    log.info(json.toJSONString());
                 }
-
-                consumer.commitAsync((offsets, exception) -> {
-                    if (exception != null) {
-                        log.error("Error commiting offsets", exception);
-                        return;
-                    }
-
-                    log.debug("Offsets commited: {}", offsets);
-                });
-
-                JSONObject json = new JSONObject(eventMap);
-                log.info(json.toJSONString());
             }
+        } catch (Exception e) {
+            log.error("Error polling data.");
+        } finally {
+            try {
+                consumer.commitSync();
+
+            } finally {
+                consumer.close();
+            }
+
         }
     }
 
